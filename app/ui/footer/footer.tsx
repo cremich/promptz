2825db1476caf7d5d566722dd4b1f5c791d0517c
Christian Bonzelet <christian.bonzelet@gmail.com>
2025-03-02 22:59:05 +0100

    refactor: footer margin

--- app/ui/footer/footer.tsx
+++ app/ui/footer/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="w-full border-t">
+    <footer className="w-full border-t mt-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col items-center justify-between py-8 sm:flex-row">
           <p className="text-sm text-gray-400">
