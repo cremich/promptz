56dd921c5d24189dcabe4ca34c9f2fc206aa14f3
Christian Bonzelet <christian.bonzelet@gmail.com>
2025-03-24 21:13:51 +0100

    feat(rules): move favorites and my prompts to user-menu

--- app/ui/navigation/navigation.ts
+++ app/ui/navigation/navigation.ts
@@ -1,6 +1,4 @@
 export const links = [
   { name: "Prompts", href: "/prompts" },
   { name: "Rules", href: "/rules" },
-  { name: "Favorites", href: "/prompts/favorites" },
-  { name: "My Prompts", href: "/prompts/my" },
 ];
